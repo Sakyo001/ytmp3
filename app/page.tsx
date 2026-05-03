ee649220bfbc9bb7a47384750fbdd20729305f88
sakyo11 <masagnayjohnroel11@gmail.com>
2026-05-03 21:34:42 +0800

    alt link

--- app/page.tsx
+++ app/page.tsx
@@ -569,8 +569,8 @@ export default function Page() {
           <ul className="alt-links-list">
             <li>
               <a
-                href="https://www.youtubemp3s.com/"
-                id="alt-link-youtubemp3s"
+                href="https://ytmp3-nu.com/"
+                id="alt-link-ytmp3-nu"
                 className="alt-link"
               >
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
